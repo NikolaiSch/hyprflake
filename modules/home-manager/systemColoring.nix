b4d1dd0fd699fa48db1af057ee8d9d18f103cd46
Nikolai Schimke <nikolai@missionctrl.dev>
2024-12-10 01:21:17 +0000

    refactor: simplify systemColoring module configuration and improve wallpaper handling

--- modules/home-manager/systemColoring.nix
+++ modules/home-manager/systemColoring.nix
@@ -1,13 +1,14 @@
-{ lib, stylix, config,  ... }:
-let 
-  cfg = config.systemColoring;
-in
 {
-  imports = [ inputs.stylix.nixosModules.stylix ];
+  lib,
+  stylix,
+  config,
+  ...
+}:
+
+{
+  imports = [ stylix.nixosModules.stylix ];
 
   options = {
-    systemColoring.enable = lib.mkEnableOption "system coloring";
-
     systemColoring.wallpaperPath = lib.mkOption {
       description = "The path to the wallpaper image.";
       type = lib.types.path;
@@ -15,15 +16,13 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    lib.mkIf cfg.wallpaperPath != null {
-      home.file.".wallpaper/wallpaper.jpg".source = cfg.wallpaperPath;
+  config = lib.pathIsRegularFile config.systemColoring.wallpaperPath {
+    home.file.".wallpaper/wallpaper.jpg".source = config.systemColoring.wallpaperPath;
 
-      stylix = {
-        enable = true;
-        image = cfg.wallpaperPath;
-        polarity = "dark";
-      };
+    stylix = {
+      enable = true;
+      image = options.systemColoring.wallpaperPath;
+      polarity = "dark";
     };
   };
 }
