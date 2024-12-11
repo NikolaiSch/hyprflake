c63322342c006236943e5edad988613c85cd36fa
Nikolai Schimke <nikolai@missionctrl.dev>
2024-12-11 00:03:14 +0000

    feat: update flake.nix to include NixOS hardware and home-manager configurations for GP62 host

--- flake.nix
+++ flake.nix
@@ -3,8 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    hardware.url = "github:nix-community/nixos-hardware";
+    nixos-hardware.url = "github:nix-community/nixos-hardware";
+
+    home-manager.url = "github:nix-community/home-manager/archive/master.tar.gz";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: { };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-hardware,
+      home-manager,
+      ...
+    }:
+    {
+      nixosConfigurations.gp62 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./hosts/gp62/default.nix ];
+      };
+
+      homeConfigurations."nikolai@gp62" = {
+        system = "x86_64-linux";
+        modules = [ ];
+      };
+    };
 }
