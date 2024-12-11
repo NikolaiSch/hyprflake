abe362b5e56df021ba0991fed568d3b92c57699a
Nikolai Schimke <schimkeNikolai@gmail.com>
2024-12-11 00:27:54 +0000

    V2 create hosts dir and upload the first (GP62 msi laptop) (#1)
    * feat: add MIT License file to the repository
    
    * feat: add hardware input to flake.nix and create default.nix and disko.nix for MSI host
    
    * feat: add default.nix and disko.nix for GP62 host configuration
    
    * feat: update flake.nix to include NixOS hardware and home-manager configurations for GP62 host
    
    * feat: update home-manager URL in flake.nix to use the latest version
    
    * feat: update flake.nix to include all modules for GP62 host configuration
    
    * feat: remove commented-out homeConfigurations for gp62 host in flake.nix
    
    * feat: update nixos-hardware URL in flake.nix to point to the master branch
    
    * feat: refactor flake.nix to inherit outputs in gp62 configuration
    
    * feat: update flake.nix to correct nixpkgs and nixos-hardware URLs and enhance gp62 configuration
    
    * feat: add RUST_BACKTRACE environment variable to flake-lock workflow and clean up flake.nix
    
    ---------
    
    Co-authored-by: Nikolai Schimke <nikolai@missionctrl.dev>

--- flake.nix
+++ flake.nix
@@ -2,12 +2,37 @@
   description = "hyprflake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    nixos-hardware.url = "github:nix-community/nixos-hardware";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    { self, nixpkgs }:
     {
-      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.hello;
+      self,
+      nixpkgs,
+      nixos-hardware,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        gp62 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/gp62
+            # "https://nix-community.github.io/home-manager/index.xhtml" # ch-nix-flakes
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              # home-manager.users.jdoe = import ./home.nix;
+            }
+          ];
+        };
+      };
     };
 }
---
+++ hosts/gp62/default.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    inputs.nixos-hardware.nixosModules.msi-gs62 # contains the unified BIOS version of `E16J9IMS`
+
+    ./disko.nix
+  ];
+}
