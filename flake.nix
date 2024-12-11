b06998b5c4f001743d2fe62b5587e19803a951cd
Nikolai Schimke <nikolai@missionctrl.dev>
2024-12-11 00:11:42 +0000

    feat: update nixos-hardware URL in flake.nix to point to the master branch

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixos-hardware.url = "github:nix-community/nixos-hardware";
+    nixos-hardware.url = "github:nix-community/nixos-hardware/master";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
