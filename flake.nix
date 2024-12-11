5d7d44a2881ae8e2465d086f0b48660f4444cfa8
Nikolai Schimke <nikolai@missionctrl.dev>
2024-12-11 00:32:57 +0000

    refactor: update nixpkgs URL format and comment out home-manager configuration

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "hyprflake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     nixos-hardware.url = "github:nix-community/nixos-hardware";
 
@@ -26,11 +26,11 @@
             ./hosts/gp62
             # "https://nix-community.github.io/home-manager/index.xhtml" # ch-nix-flakes
             home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              # home-manager.users.jdoe = import ./home.nix;
-            }
+            # {
+            #   home-manager.useGlobalPkgs = true;
+            #   home-manager.useUserPackages = true;
+            #   home-manager.users.vii = import ./vii.nix;
+            # }
           ];
         };
       };
