f90cfa1a787347906bcdac033e32c6afa5f7146b
Nik <Nik@sch.ee>
2024-12-12 18:51:18 +0000

    Adjusts default configurations
    Sets Hyprland as the default desktop environment
    Updates laptop detection logic
    Revises Darwin system configuration defaults
    
    Relates to system customization improvements

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -8,7 +8,7 @@
   mkHome = {
     hostname,
     username ? "vii",
-    desktop ? "",
+    desktop ? "hyprland",
     platform ? "x86_64-linux",
   }: let
     isISO = builtins.substring 0 4 hostname == "iso-";
@@ -40,15 +40,15 @@
   mkNixos = {
     hostname,
     username ? "vii",
-    desktop ? "",
+    desktop ? "hyprland",
     platform ? "x86_64-linux",
   }: let
     isISO = builtins.substring 0 4 hostname == "iso-";
     isInstall = !isISO;
     isLaptop =
-      hostname != "msi-gp62";
+      hostname == "msi-gp62";
     isWorkstation = "" != desktop;
-    tailNet = "drongo-gamma.ts.net";
+    tailNet = "vii.net";
   in
     inputs.nixpkgs.lib.nixosSystem {
       specialArgs = {
@@ -80,36 +80,36 @@
         ++ inputs.nixpkgs.lib.optionals isISO [cd-dvd];
     };
 
-  # mkDarwin =
-  #   {
-  #     desktop ? "aqua",
-  #     hostname,
-  #     username ? "martin",
-  #     platform ? "aarch64-darwin",
-  #   }:
-  #   let
-  #     isISO = false;
-  #     isInstall = true;
-  #     isLaptop = true;
-  #     isWorkstation = true;
-  #   in
-  #   inputs.nix-darwin.lib.darwinSystem {
-  #     specialArgs = {
-  #       inherit
-  #         inputs
-  #         outputs
-  #         desktop
-  #         hostname
-  #         platform
-  #         username
-  #         isInstall
-  #         isISO
-  #         isLaptop
-  #         isWorkstation
-  #         ;
-  #     };
-  #     modules = [ ../darwin ];
-  #   };
+  mkDarwin =
+    {
+      desktop,
+      hostname ? "nikbook",
+      username ? "vii",
+      platform ? "aarch64-darwin",
+    }:
+    let
+      isISO = false;
+      isInstall = true;
+      isLaptop = true;
+      isWorkstation = true;
+    in
+    inputs.nix-darwin.lib.darwinSystem {
+      specialArgs = {
+        inherit
+          inputs
+          outputs
+          desktop
+          hostname
+          platform
+          username
+          isInstall
+          isISO
+          isLaptop
+          isWorkstation
+          ;
+      };
+      modules = [ ../darwin ];
+    };
 
   forAllSystems = inputs.nixpkgs.lib.genAttrs [
     "aarch64-linux"
