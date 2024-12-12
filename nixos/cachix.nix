15332472202e2adefb5bf96262e2043dcb6a976a
Nik <Nik@sch.ee>
2024-12-12 18:33:40 +0000

    Removes Hyprland Cachix configuration
    Eliminates the need for external substituters and trusted keys, simplifying the Nix configuration

--- nixos/cachix.nix
+++
@@ -1,6 +0,0 @@
-{
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org" "https://hyprflake.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "hyprflake.cachix.org-1:7fpoBMQuosbaoqxQ5bsxR2GPVQ3mjBp7dfziXc4e+hM="];
-  };
-}
