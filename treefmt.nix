f19d87a2a739d1c4b0f4fd305a5a8c7f910ce9b2
Nik <Nik@sch.ee>
2024-12-12 13:46:00 +0000

    alter

--- treefmt.nix
+++ treefmt.nix
@@ -1,10 +1,8 @@
 { pkgs, ... }:
 {
-        projectRootFile = "LICENSE.md";
         programs.nixfmt.enable = pkgs.lib.meta.availableOn pkgs.stdenv.buildPlatform pkgs.nixfmt-rfc-style.compiler;
         programs.nixfmt.package = pkgs.nixfmt-rfc-style;
         programs.shellcheck.enable = true;
-        programs.deno.enable = true;
         settings.formatter.shellcheck.options = [
           "-s"
           "bash"
