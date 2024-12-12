8797f7a072f277a15786e8d51c3cdac7dd63d8f8
Nik <Nik@sch.ee>
2024-12-12 14:41:38 +0000

    testing pre-commit

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
 
       systems = [
         # systems for which you want to build the `perSystem` attributes
-        "x86_64-linux"
+          "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
