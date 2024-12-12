738f7435c7e383c7d16931a3eaba9ef5ec5701c7
Nik <Nik@sch.ee>
2024-12-12 14:41:22 +0000

    adding files

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
 
     treefmt-nix.url = "github:numtide/treefmt-nix";
 
-
     pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -28,6 +27,7 @@
       home-manager,
       flake-parts,
       treefmt-nix,
+      nixos-hardware,
       ...
     }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -41,9 +41,10 @@
           "msi-gp62" = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
+              nixos-hardware.nixosModules.msi-gl62
+              home-manager.nixosModules.home-manager
               ./hosts/msi-gp62
               # "https://nix-community.github.io/home-manager/index.xhtml" # ch-nix-flakes
-              home-manager.nixosModules.home-manager
             ];
           };
         };
@@ -58,7 +59,12 @@
       ];
 
       perSystem =
-        { pkgs, config, system,  ... }:
+        {
+          pkgs,
+          config,
+          system,
+          ...
+        }:
         let
           treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
         in
@@ -68,13 +74,12 @@
           checks = {
             formatting = treefmtEval.config.build.check self;
             pre-commit = inputs.pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = import ./pre-commit.nix;
-          };
+              src = ./.;
+              hooks = import ./pre-commit.nix;
+            };
           };
 
           treefmt = import ./treefmt.nix { inherit pkgs; };
-
 
         };
     };
--- hosts/msi-gp62/default.nix
+++ hosts/msi-gp62/default.nix
@@ -5,6 +5,9 @@
 {
   imports = [
     inputs.nixos-hardware.nixosModules.msi-gs62 # contains the unified BIOS version of `E16J9IMS`
-    ./disko.nix
   ];
+
+  disk = 
+    ./disko.nix
+;
 }
